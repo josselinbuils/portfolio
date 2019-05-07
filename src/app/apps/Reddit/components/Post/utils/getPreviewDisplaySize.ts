80d8b888717a67b1878fc0c1e1969cad02b9d65d
Josselin BUILS <josselin.buils@gmail.com>
2019-05-07 23:29:33 +0200

    Improves display image size computation

--- src/app/apps/Reddit/components/Post/utils/getPreviewDisplaySize.ts
+++ src/app/apps/Reddit/components/Post/utils/getPreviewDisplaySize.ts
@@ -1,16 +1,11 @@
 import { Size } from '~/platform/interfaces';
 
-const PREVIEW_MAX_HEIGHT = 300;
+const PREVIEW_MAX_HEIGHT = 270;
 const PREVIEW_MAX_WIDTH = 470;
 
 export function getPreviewDisplaySize(width: number, height: number): Size {
-  let displayWidth = width;
-  let displayHeight = height;
-
-  if (displayWidth > PREVIEW_MAX_WIDTH) {
-    displayWidth = PREVIEW_MAX_WIDTH;
-    displayHeight = Math.round((height / width) * displayWidth);
-  }
+  let displayWidth = PREVIEW_MAX_WIDTH;
+  let displayHeight = Math.round((height / width) * displayWidth);
 
   if (displayHeight > PREVIEW_MAX_HEIGHT) {
     displayHeight = PREVIEW_MAX_HEIGHT;
