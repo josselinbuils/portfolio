e986cf66e8d625a95d1e37f0e8cc6eabb27f7c99
Josselin BUILS <josselin.buils@gmail.com>
2019-05-15 23:33:48 +0200

    Adds util to avoid trying to set state of unmounted components

--- src/app/apps/Reddit/components/Icon/Icon.tsx
+++ src/app/apps/Reddit/components/Icon/Icon.tsx
@@ -1,5 +1,6 @@
 import cn from 'classnames';
 import React, { FC, useEffect, useState } from 'react';
+import { cancelable } from '~/platform/utils';
 import { getIconSrc } from './utils';
 import styles from './Icon.module.scss';
 
@@ -7,8 +8,12 @@ export const Icon: FC<Props> = ({ active = true, subreddit }) => {
   const [src, setSrc] = useState<string>();
 
   useEffect(() => {
+    const [srcPromise, cancelSrcPromise] = cancelable(getIconSrc(subreddit));
+
     setSrc(undefined);
-    getIconSrc(subreddit).then(setSrc);
+    srcPromise.then(setSrc);
+
+    return cancelSrcPromise;
   }, [subreddit]);
 
   return src ? (
---
+++ src/app/platform/utils/cancelable.ts
@@ -0,0 +1,22 @@
+import { Deferred } from '@josselinbuils/utils';
+
+export function cancelable<T>(
+  basePromise: Promise<T>
+): [Promise<T>, () => void] {
+  const { promise, reject, resolve } = new Deferred<T>();
+  let active = true;
+
+  basePromise
+    .then(value => {
+      if (active) {
+        resolve(value);
+      }
+    })
+    .catch(reason => {
+      if (active) {
+        reject(reason);
+      }
+    });
+
+  return [promise, () => (active = false)];
+}
