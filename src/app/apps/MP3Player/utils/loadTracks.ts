50a088e1ba87a52e68d2933f8ad0c1d0745c3fbb
Josselin BUILS <josselin.buils@gmail.com>
2019-05-21 13:37:37 +0200

    Adds loadTracks util to MP3Player

---
+++ src/app/apps/MP3Player/utils/loadTracks.ts
@@ -0,0 +1,19 @@
+import dayjs from 'dayjs';
+import { BASE_URL } from '~/platform/constants';
+import { Music } from '../interfaces';
+
+// TODO format musics in the backend
+
+export async function loadTracks(
+  path: string,
+  order: string = 'popularity_total'
+): Promise<Music[]> {
+  const response = await fetch(`${BASE_URL}/api/jamendo${path}/${order}`);
+  const musicList = await response.json();
+
+  musicList.forEach((music: any) => {
+    music.readableDuration = dayjs(music.duration * 1000).format('mm:ss');
+  });
+
+  return musicList;
+}
