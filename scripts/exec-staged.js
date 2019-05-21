88bee745ff53d055c79873aaad9e49f474565f1f
Josselin BUILS <josselin.buils@gmail.com>
2019-05-21 20:45:01 +0200

    Fixes exec-staged script to not handle all modified files

--- scripts/exec-staged.js
+++ scripts/exec-staged.js
@@ -3,7 +3,7 @@ const { execSync } = require('child_process');
 for (let i = 2; i < process.argv.length; i += 2) {
   const command = process.argv[i];
   const regex = process.argv[i + 1];
-  const execCommand = `LIST=\`git diff-index --name-only --diff-filter=d HEAD | grep -E "${regex}"\`; if [ "$LIST" ]; then echo ${command} $LIST; ${command} $LIST; fi`;
+  const execCommand = `LIST=\`git diff-index --cached --name-only --diff-filter=d HEAD | grep -E "${regex}"\`; if [ "$LIST" ]; then echo ${command} $LIST; ${command} $LIST; fi`;
 
   try {
     execSync(execCommand, {
