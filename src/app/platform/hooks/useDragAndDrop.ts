f7677cf3e74518b8fbee0f4bdd5dc8f5ccb11813
Josselin BUILS <josselin.buils@gmail.com>
2020-02-14 20:54:36 +0100

    Removes global event listeners in useDragAndDrop

--- src/app/platform/hooks/useDragAndDrop.ts
+++ src/app/platform/hooks/useDragAndDrop.ts
@@ -1,25 +1,12 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { MouseButton } from '~/platform/constants';
 import { EventHandler } from '~/platform/interfaces';
 import { noop } from '~/platform/utils';
-import { useEventListener } from './useEventListener';
 
-// TODO remove global event listeners
 export function useDragAndDrop(
   downHandler: (downEvent: React.MouseEvent) => MouseMoveHandler | void,
   upHandler: MouseUpHandler = noop
 ): (downEvent: React.MouseEvent) => void {
-  const moveHandlerRef = useRef<MouseMoveHandler>(noop);
-  const upHandlerRef = useRef<MouseUpHandler>(noop);
-
-  useEventListener('mousemove', (moveEvent: MouseEvent) =>
-    requestAnimationFrame(() => moveHandlerRef.current(moveEvent))
-  );
-
-  useEventListener('mouseup', (upEvent: MouseEvent) =>
-    upHandlerRef.current(upEvent)
-  );
-
   return function dragAndDropHandler(downEvent: React.MouseEvent): void {
     if (downEvent.button !== MouseButton.Left) {
       return;
@@ -32,13 +19,14 @@ export function useDragAndDrop(
       downEvent.preventDefault();
       downEvent.persist();
 
-      moveHandlerRef.current = moveHandler;
-
-      upHandlerRef.current = (upEvent: MouseEvent) => {
+      const hookUpHandler = (upEvent: MouseEvent) => {
+        window.removeEventListener('mousemove', moveHandler);
+        window.removeEventListener('mouseup', hookUpHandler);
         upHandler(upEvent);
-        moveHandlerRef.current = noop;
-        upHandlerRef.current = noop;
       };
+
+      window.addEventListener('mousemove', moveHandler);
+      window.addEventListener('mouseup', hookUpHandler);
     }
   };
 }
