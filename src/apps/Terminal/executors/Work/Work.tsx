0f1c1c97fc1a8cbece9430613ba557f10b4df406
Josselin BUILS <josselin.buils@gmail.com>
2020-03-28 22:38:07 +0100

    Updates work executor

--- src/apps/Terminal/executors/Work/Work.tsx
+++ src/apps/Terminal/executors/Work/Work.tsx
@@ -7,20 +7,7 @@ export const Work: Executor = () => (
   <>
     <div className={styles.entry}>
       <p className={styles.header}>
-        <span>Software Engineer Intern at GE Healthcare</span>
-        <span className={styles.location}>(Buc, France)</span>
-        <span className={styles.period}>mar 2015 -> sept 2015</span>
-      </p>
-      <p className={styles.description}>
-        I implemented an innovative HMI using a web-based architecture to
-        optimize the ergonomics of an interventional X-Ray imaging system by
-        incorporating the ability to interact with other related medical
-        devices.
-      </p>
-    </div>
-    <div className={styles.entry}>
-      <p className={styles.header}>
-        <span>Software Engineer at GE Healthcare</span>
+        <span>Software Engineer at General Electric Healthcare</span>
         <span className={styles.location}>(Buc, France)</span>
         <span className={styles.period}>oct 2015 -> nov 2018</span>
       </p>
