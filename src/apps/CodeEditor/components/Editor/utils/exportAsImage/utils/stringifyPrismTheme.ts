9c1de834ad9c985fb58c0bff157fc4586207fd66
Josselin BUILS <josselin.buils@gmail.com>
2020-04-26 12:34:59 +0200

    Fixes Prism theme stringify in prod

--- src/apps/CodeEditor/components/Editor/utils/exportAsImage/utils/stringifyPrismTheme.ts
+++ src/apps/CodeEditor/components/Editor/utils/exportAsImage/utils/stringifyPrismTheme.ts
@@ -1,19 +1,19 @@
 export function stringifyPrismTheme(highlightedCode: string): string {
-  const selectorMatch = highlightedCode.match(/class="([^ ]+)/);
+  const selectorMatch = highlightedCode.match(/class="[^ ]+ ([^ "]+)/);
 
   // No highlighted parts
   if (selectorMatch === null) {
     return '';
   }
 
-  const selector = `.${selectorMatch[1]}`;
+  const selector = selectorMatch[1];
   const styleSheets = Array.from(document.styleSheets) as CSSStyleSheet[];
   const rules = styleSheets.find((sheet) =>
-    Array.from(sheet.rules).some(({ cssText }) => cssText.startsWith(selector))
+    Array.from(sheet.rules).some(({ cssText }) => cssText.includes(selector))
   );
 
   if (rules === undefined) {
-    throw new Error(`Unable to find Prism theme using selector ${selector}`);
+    throw new Error(`Unable to find Prism theme using selector '${selector}'`);
   }
 
   return Array.from(rules.cssRules)
