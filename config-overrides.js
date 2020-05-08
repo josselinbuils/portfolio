4c172814186d918d91c5d23fefc1145de91e35a2
Josselin BUILS <josselin.buils@gmail.com>
2020-05-08 18:15:25 +0200

    Adds a Webpack plugin to prevent adding circular dependencies

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,14 @@
+const CircularDependencyPlugin = require('circular-dependency-plugin');
 const path = require('path');
 
 module.exports = (config) => {
+  config.plugins.push(
+    new CircularDependencyPlugin({
+      exclude: /node_modules/,
+      failOnError: true,
+      cwd: process.cwd(),
+    })
+  );
   config.resolve = {
     ...config.resolve,
     alias: {
@@ -13,6 +21,8 @@ module.exports = (config) => {
     const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
     config.plugins.push(new BundleAnalyzerPlugin());
   }
+
+  console.log(process.env);
 
   return config;
 };
