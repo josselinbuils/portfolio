3d672ae537e88da738f075c3ec5d35e09fd78e30
Josselin BUILS <josselin.buils@gmail.com>
2020-06-14 15:48:14 +0200

    Puts back bundle analysis

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-module.exports = {
+let config = {
   assetPrefix: process.env.HTTP_PREFIX || '',
   publicRuntimeConfig: {
     basePath: process.env.HTTP_PREFIX || '',
   },
 };
+
+if (process.env.ANALYZE === 'true') {
+  const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+  });
+  config = withBundleAnalyzer(config);
+}
+
+module.exports = config;
