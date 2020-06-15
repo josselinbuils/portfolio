1e159f1774ce281b1c09eba60730e0ed396bda42
Josselin BUILS <josselin.buils@gmail.com>
2020-06-15 21:09:55 +0200

    Puts back circular dependency plugin

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const CircularDependencyPlugin = require('circular-dependency-plugin');
 const { homepage } = require('./package.json');
 
 const { pathname } = new URL(homepage);
@@ -10,10 +11,22 @@ let config = {
     basePath: basePath,
     productionBrowserSourceMaps: true,
   },
+  webpack: (config) => {
+    config.plugins.push(
+      new CircularDependencyPlugin({
+        allowAsyncCycles: false,
+        exclude: /node_modules/,
+        failOnError: true,
+        cwd: process.cwd(),
+      })
+    );
+    return config;
+  },
 };
 
 if (process.env.ANALYZE === 'true') {
-  const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  const bundleAnalyzer = require('@next/bundle-analyzer');
+  const withBundleAnalyzer = bundleAnalyzer({
     enabled: process.env.ANALYZE === 'true',
   });
   config = withBundleAnalyzer(config);
