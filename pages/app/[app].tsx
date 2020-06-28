498ccb4245a369b0f158fc74838d500e62f40fa1
Josselin BUILS <josselin.buils@gmail.com>
2020-06-28 23:46:37 +0200

    Test not using Home component for index route

--- pages/app/[app].tsx
+++ pages/app/[app].tsx
@@ -1,10 +1,7 @@
-import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { FC, useEffect } from 'react';
-import { Desktop } from '~/platform/components/Desktop';
+import { Home } from '~/platform/components/Home';
 import { useInjector } from '~/platform/hooks/useInjector';
-import { ContextMenuProvider } from '~/platform/providers/ContextMenuProvider/ContextMenuProvider';
-import { TooltipProvider } from '~/platform/providers/TooltipProvider/TooltipProvider';
 import { WindowManager } from '~/platform/services/WindowManager';
 import { getAppDescriptors } from '~/platform/utils/getAppDescriptors';
 
@@ -23,59 +20,7 @@ const App: FC = () => {
     }
   }, [app, windowManager]);
 
-  return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
-        <title>Josselin BUILS</title>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <meta
-          name="description"
-          content="Hey, I'm Josselin, a full-stack JavaScript developer :)"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: `\
-    {
-      '@context': 'http://schema.org/',
-      '@type': 'Person',
-      jobTitle: 'Software Engineer',
-      name: 'Josselin BUILS',
-      nationality: 'French',
-    }`,
-          }}
-        />
-        <script
-          async
-          src="https://polyfill.io/v3/polyfill.min.js?features=ResizeObserver"
-        />
-      </Head>
-      <ContextMenuProvider>
-        <TooltipProvider>
-          <Desktop />
-        </TooltipProvider>
-      </ContextMenuProvider>
-    </>
-  );
+  return <Home />;
 };
 
 export default App;
