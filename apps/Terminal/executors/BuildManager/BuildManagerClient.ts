051a0c137405b9ef522158b5c3bd01b489b7fdd3
Josselin BUILS <josselin.buils@gmail.com>
2020-10-04 08:18:45 +0200

    Adds retry mechanism in BuildManagerClient

--- apps/Terminal/executors/BuildManager/BuildManagerClient.ts
+++ apps/Terminal/executors/BuildManager/BuildManagerClient.ts
@@ -2,6 +2,8 @@ import { Deferred } from '@josselinbuils/utils';
 import { PROD_HOSTNAME } from '~/platform/constants';
 import { noop } from '~/platform/utils/noop';
 
+const RETRY_DELAY_MS = 2000;
+
 export enum MessageType {
   AuthToken = 'authToken',
   Command = 'command',
@@ -16,18 +18,25 @@ export class BuildManagerClient {
   private messageHandler = noop as (message: BMMessage) => void;
   private readonly readyDeferred = new Deferred<BuildManagerClient>();
   private readonly ws: WebSocket;
+  private retryTimeout?: number;
 
   constructor() {
     const ws = new WebSocket(`wss://${PROD_HOSTNAME}/build-manager`);
 
-    ws.onclose = () => this.closeHandler();
+    ws.onclose = () => {
+      this.clearRetryTimeout();
+      this.closeHandler();
+    };
 
     ws.onerror = () => {
+      this.clearRetryTimeout();
       this.readyDeferred.reject();
       this.errorHandler();
     };
 
     ws.onmessage = (event) => {
+      this.clearRetryTimeout();
+
       try {
         const message = JSON.parse(event.data) as BMMessage;
 
@@ -63,7 +72,13 @@ export class BuildManagerClient {
 
   send(type: MessageType, value: any): BuildManagerClient {
     if (this.ws.readyState === this.ws.OPEN) {
-      this.ws.send(JSON.stringify({ type, value }));
+      const message = JSON.stringify({ type, value });
+      this.ws.send(message);
+      this.clearRetryTimeout();
+      this.retryTimeout = window.setTimeout(
+        () => this.ws.send(message),
+        RETRY_DELAY_MS
+      );
     }
     return this;
   }
@@ -76,6 +91,13 @@ export class BuildManagerClient {
 
   async waitUntilReady(): Promise<BuildManagerClient> {
     return this.readyDeferred.promise;
+  }
+
+  private clearRetryTimeout(): void {
+    if (this.retryTimeout !== undefined) {
+      window.clearTimeout(this.retryTimeout);
+      delete this.retryTimeout;
+    }
   }
 }
 
