4b467cd8e6578c55da1f4cab8ec09d6a84dc8a5c
Josselin BUILS <josselin.buils@gmail.com>
2020-11-01 11:29:05 +0100

    Adds cache on blog post methods that read file system

--- apps/Blog/utils/getBlogPost.ts
+++ apps/Blog/utils/getBlogPost.ts
@@ -2,15 +2,23 @@ import { BlogPost } from '../interfaces/BlogPost';
 import { getPostSlug } from './getPostSlug';
 import { getPostTitle } from './getPostTitle';
 
+const cachedBlogPosts = {} as { [slug: string]: BlogPost };
+
 export function getBlogPost(slug: string): BlogPost {
-  const context = require.context('../posts', false, /\.md$/);
-  const postKey = context.keys().find((key) => getPostSlug(key) === slug);
+  if (
+    cachedBlogPosts[slug] === undefined ||
+    process.env.NODE_ENV !== 'production'
+  ) {
+    const context = require.context('../posts', false, /\.md$/);
+    const postKey = context.keys().find((key) => getPostSlug(key) === slug);
 
-  if (postKey === undefined) {
-    throw new Error(`Unable to find post with slug ${slug}.`);
+    if (postKey === undefined) {
+      throw new Error(`Unable to find post with slug ${slug}.`);
+    }
+    const content = context(postKey).default as string;
+    const title = getPostTitle(content);
+
+    cachedBlogPosts[slug] = { content, title, slug };
   }
-  const content = context(postKey).default as string;
-  const title = getPostTitle(content);
-
-  return { content, title, slug };
+  return cachedBlogPosts[slug];
 }
--- apps/Blog/utils/getBlogPosts.ts
+++ apps/Blog/utils/getBlogPosts.ts
@@ -2,11 +2,16 @@ import { BlogPostMetadata } from '../interfaces/BlogPostMetadata';
 import { getPostSlug } from './getPostSlug';
 import { getPostTitle } from './getPostTitle';
 
-export function getBlogPosts(): BlogPostMetadata[] {
-  const context = require.context('../posts', false, /\.md$/);
+let cachedBlogPosts: BlogPostMetadata[];
 
-  return context.keys().map((key) => ({
-    slug: getPostSlug(key),
-    title: getPostTitle(context(key).default),
-  }));
+export function getBlogPosts(): BlogPostMetadata[] {
+  if (cachedBlogPosts === undefined || process.env.NODE_ENV !== 'production') {
+    const context = require.context('../posts', false, /\.md$/);
+
+    cachedBlogPosts = context.keys().map((key) => ({
+      slug: getPostSlug(key),
+      title: getPostTitle(context(key).default),
+    }));
+  }
+  return cachedBlogPosts;
 }
