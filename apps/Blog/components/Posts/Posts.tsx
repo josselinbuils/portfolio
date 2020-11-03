be3cf1fbd3cdfacbd75b18de3d6e8a941c287b88
Josselin BUILS <josselin.buils@gmail.com>
2020-11-03 22:05:28 +0100

    Adds social links in blog

--- apps/Blog/components/Posts/Posts.tsx
+++ apps/Blog/components/Posts/Posts.tsx
@@ -12,6 +12,10 @@ Hey, I'm Josselin, a full-stack JavaScript developer 😄
 
 Here are some posts where I give my opinion on code stuff 👨‍💻
 
+[![{ "alt": "LinkedIn", "height": 32, "loading": "eager", "width": 32 }](/blog/linkedin.svg)](https://www.linkedin.com/in/josselinbuils)
+[![{ "alt": "Twitter", "height": 32, "loading": "eager", "style": { "marginLeft": "1.5rem" }, "width": 32 }](/blog/twitter.svg)](https://twitter.com/josselinbuils)
+[![{ "alt": "Twitter", "height": 32, "loading": "eager", "style": { "marginLeft": "1.5rem" }, "width": 32 }](/blog/github.svg)](https://github.com/josselinbuils)
+
 ## Posts
 
 ${posts.map(({ slug, title }) => `- [${title}](/blog/${slug})`).join('\\n')}
