ec27fa0abec07954eb0802e86610580029b4d6c8
Josselin BUILS <josselin.buils@gmail.com>
2023-08-04 23:11:58 +0200

    feat(build): customise chunk and class names

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import crypto from 'node:crypto';
 import path from 'node:path';
 import preact from '@preact/preset-vite';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import { vitePluginPage } from './config/vitePluginPage';
+
+const cssHashMap = new Map<string, string>();
 
 // https://vitejs.dev/config/
 export default defineConfig(({ ssrBuild }) => ({
@@ -19,8 +22,26 @@ export default defineConfig(({ ssrBuild }) => ({
           warn(log);
         }
       },
+      output: {
+        assetFileNames: 'assets/[name]-[hash:6].[ext]',
+        chunkFileNames: 'assets/[name]-[hash:6].js',
+        entryFileNames: 'assets/[name]-[hash:6].js',
+      },
     },
     sourcemap: true,
+  },
+  css: {
+    modules: {
+      generateScopedName(name, _, css) {
+        if (!cssHashMap.has(css)) {
+          cssHashMap.set(
+            css,
+            crypto.createHash('md5').update(css).digest('hex').slice(0, 6),
+          );
+        }
+        return `${name}_${cssHashMap.get(css)}`;
+      },
+    },
   },
   plugins: [
     vitePluginPage(),
